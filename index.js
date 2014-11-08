5702b631d84b5a20a80ccc9548c4db38179414ae
Joe <joe@chetcodigital.com>
2014-11-07 18:05:19 -0800

    tcp server duid

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ var influx = require('influx');
 							console.log('pushsmartinit:' + pushsmartuid + '\r\n');
 							// need to put in UID check here to be sure all characters are Hex types.
 							//var isHex = pushsmartuid.match("[0-9A-F]+");
-							PSUIDstr = str(pushsmartuid);
+							PSUIDstr = toString(pushsmartuid);
 							if(PSUIDstr.match(/^[0-9A-F]{40}&/i) == null)
 							{
 								console.log('pushsmartinit: error .... DUID invalid Hex String \r\n');
